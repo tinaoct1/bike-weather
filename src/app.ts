85dd2b8dbda6431f70155e60097c7ad9bb0f33f5
coder_0110 <tinaoct1@gmail.com>
2020-09-22 08:59:21 +0400

    added cors

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,12 @@ import { Application } from 'express';
 import * as bodyParser from 'body-parser';
 import { MainRouter } from './routes/routes';
 import { loadErrorHandlers } from './utilities/error-handling';
+import cors from 'cors';
 
 import './db';// initialize database
 
 const app: Application = express();
-
+app.use(cors())
 app.use(bodyParser.json());
 
 app.use('/api/v1/', MainRouter);
