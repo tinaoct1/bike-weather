8c4c1a8e45f47b5caa356dd2dd9739450386e214
coder_0110 <tinaoct1@gmail.com>
2020-09-22 12:03:20 +0400

    fix in axios post request headers

--- src/utilities/data-collector.ts
+++ src/utilities/data-collector.ts
@@ -2,8 +2,13 @@ import axios from 'axios';
 import {API_HOST, API_TOKEN} from './secrets';
 
 export async function collectData() {
-    await axios.post(`${API_HOST}/indego-data-fetch-and-store-it-db`, {
-        'Content-Type': 'application/json',
-        'api-key': API_TOKEN
-    });
+    await axios.post(`${API_HOST}/indego-data-fetch-and-store-it-db`,
+        {},
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                'api-key': API_TOKEN
+            }
+        }
+    )
 }
