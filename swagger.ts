59a4ef2fcd797d1ca10c2c61711207026053ac18
coder_0110 <tinaoct1@gmail.com>
2020-09-23 11:13:11 +0400

    updated swagger

--- swagger.ts
+++ swagger.ts
@@ -1,10 +1,9 @@
 export const swaggerDocument = {
   openapi: '3.0.1',
   info: {
+    title: 'Bike and Weather',
     description:
       'Get a historical snapshot of bike stations and weather conditions in Philadelphia',
-    version: '1.0.0',
-    title: 'Bike and Weather',
     contact: {
       email: 'tinaoct1@gmail.com',
     },
@@ -12,14 +11,22 @@ export const swaggerDocument = {
       name: 'Apache 2.0',
       url: 'http://www.apache.org/licenses/LICENSE-2.0.html',
     },
+    version: '1.0.0',
   },
-  host: 'bike-weather.herokuapp.com',
+  externalDocs: {
+    description: 'Find out more about Swagger',
+    url: 'http://swagger.io',
+  },
+  servers: [
+    {
+      url: '/bike-weather.herokuapp.com/',
+    },
+  ],
   paths: {
     '/api/v1/stations': {
       get: {
         tags: ['stations-and-weather'],
         summary: 'Get a snapshot of stations and weather',
-        produces: ['application/json'],
         parameters: [
           {
             name: 'at',
@@ -27,21 +34,63 @@ export const swaggerDocument = {
             description:
               'The time for which the historical data has to be obtained in UTC format eg:2020-09-21T07:00:00',
             required: true,
-            type: 'string',
+            schema: {
+              type: 'string',
+            },
           },
           {
             name: 'api-token',
             in: 'header',
             required: true,
-            type: 'string',
+            schema: {
+              type: 'string',
+            },
           },
         ],
         responses: {
           '200': {
-            description: 'Success',
+            description: 'Successfully fetched stations and weather',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    stations: {
+                      type: 'array',
+                      items: {
+                        $ref: '#/components/schemas/station',
+                      },
+                    },
+                    weather: {
+                      $ref: '#/components/schemas/weather',
+                    },
+                    at: {
+                      $ref: '#/components/schemas/at',
+                    },
+                  },
+                },
+              },
+            },
           },
-          '404': {
-            description: 'Data not found',
+          '400': {
+            description: 'Bad Request',
+            content: {
+              'text/plain': {
+                schema: {
+                  type: 'string',
+                },
+              },
+            },
+          },
+          '403': {
+            description: 'Forbidden(api-token not passed)',
+            content: {
+              'text/plain': {
+                schema: {
+                  type: 'string',
+                },
+              },
+            },
           },
         },
       },
@@ -51,15 +100,16 @@ export const swaggerDocument = {
         tags: ['station-and-weather'],
         summary: 'Get station and weather details by kioskId',
         description: 'Returns a single station',
-        produces: ['application/json'],
         parameters: [
           {
             name: 'kioskId',
             in: 'path',
             description: 'ID of kiosk to fetch',
             required: true,
-            type: 'integer',
-            format: 'int64',
+            schema: {
+              type: 'integer',
+              format: 'int64',
+            },
           },
           {
             name: 'at',
@@ -67,21 +117,65 @@ export const swaggerDocument = {
             description:
               'The time for which the historical data has to be obtained in UTC format eg:2020-09-21T07:00:00',
             required: true,
-            type: 'string',
+            schema: {
+              type: 'string',
+            },
           },
           {
             name: 'api-token',
             in: 'header',
             required: true,
-            type: 'string',
+            schema: {
+              type: 'string',
+            },
           },
         ],
         responses: {
           '200': {
-            description: 'successful operation',
+            description: 'Successfully fetched stations and weather',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'object',
+                  properties: {
+                    stations: {
+                      type: 'array',
+                      items: {
+                        $ref: '#/components/schemas/station',
+                        minItems: 1,
+                        maxItems: 1,
+                      },
+                    },
+                    weather: {
+                      $ref: '#/components/schemas/weather',
+                    },
+                    at: {
+                      $ref: '#/components/schemas/at',
+                    },
+                  },
+                },
+              },
+            },
           },
-          '404': {
-            description: 'Kiosk not found',
+          '400': {
+            description: 'Bad Request',
+            content: {
+              'text/plain': {
+                schema: {
+                  type: 'string',
+                },
+              },
+            },
+          },
+          '403': {
+            description: 'Forbidden(api-token not passed)',
+            content: {
+              'text/plain': {
+                schema: {
+                  type: 'string',
+                },
+              },
+            },
           },
         },
       },
@@ -91,24 +185,348 @@ export const swaggerDocument = {
         tags: ['create-snapshot-of-weather-and-stations'],
         summary:
           'Creates a snapshot of the weather and stations info at the time this api is called',
-        description: '',
-        produces: ['application/json'],
-        parameters: {
-          name: 'api-token',
-          in: 'header',
-          required: true,
-          type: 'string',
-        },
+        parameters: [
+          {
+            name: 'api-token',
+            in: 'header',
+            required: true,
+            schema: {
+              type: 'string',
+            },
+          },
+        ],
         responses: {
-          default: {
-            description: 'successful operation',
+          '200': {
+            description: 'Successfully added stations and weather',
+            content: {
+              'text/plain': {
+                schema: {
+                  type: 'string',
+                },
+              },
+            },
           },
         },
       },
     },
   },
-  externalDocs: {
-    description: 'Find out more about Swagger',
-    url: 'http://swagger.io',
+  components: {
+    schemas: {
+      station: {
+        type: 'object',
+        properties: {
+          geometry: {
+            type: 'object',
+            properties: {
+              coordinates: {
+                maxItems: 2,
+                minItems: 2,
+                type: 'array',
+                items: {
+                  type: 'number',
+                },
+              },
+            },
+          },
+          properties: {
+            type: 'object',
+            properties: {
+              coordinates: {
+                maxItems: 2,
+                minItems: 2,
+                type: 'array',
+                items: {
+                  type: 'number',
+                },
+              },
+              id: {
+                type: 'integer',
+                format: 'int32',
+              },
+              name: {
+                type: 'string',
+              },
+              totalDocks: {
+                type: 'integer',
+                format: 'int32',
+              },
+              docksAvailable: {
+                type: 'integer',
+                format: 'int32',
+              },
+              bikesAvailable: {
+                type: 'integer',
+                format: 'int32',
+              },
+              classicBikesAvailable: {
+                type: 'integer',
+                format: 'int32',
+              },
+              smartBikesAvailable: {
+                type: 'integer',
+                format: 'int32',
+              },
+              electricBikesAvailable: {
+                type: 'integer',
+                format: 'int32',
+              },
+              rewardBikesAvailable: {
+                type: 'integer',
+                format: 'int32',
+              },
+              rewardDocksAvailable: {
+                type: 'integer',
+                format: 'int32',
+              },
+              kioskStatus: {
+                type: 'string',
+              },
+              kioskPublicStatus: {
+                type: 'string',
+              },
+              kioskConnectionStatus: {
+                type: 'string',
+              },
+              kioskType: {
+                type: 'integer',
+                format: 'int32',
+              },
+              addressStreet: {
+                type: 'string',
+              },
+              addressCity: {
+                type: 'string',
+              },
+              addressState: {
+                type: 'string',
+              },
+              addressZipCode: {
+                type: 'string',
+              },
+              bikes: {
+                type: 'array',
+                items: {
+                  type: 'object',
+                  properties: {
+                    _id: {
+                      type: 'string',
+                    },
+                    dockNumber: {
+                      type: 'integer',
+                      format: 'int32',
+                    },
+                    isElectric: {
+                      type: 'boolean',
+                    },
+                    isAvailable: {
+                      type: 'boolean',
+                    },
+                    battery: {
+                      type: 'object',
+                    },
+                  },
+                },
+              },
+              closeTime: {
+                type: 'object',
+              },
+              eventEnd: {
+                type: 'object',
+              },
+              eventStart: {
+                type: 'object',
+              },
+              isEventBased: {
+                type: 'boolean',
+              },
+              isVirtual: {
+                type: 'boolean',
+              },
+              kioskId: {
+                type: 'integer',
+                format: 'int32',
+              },
+              notes: {
+                type: 'object',
+              },
+              openTime: {
+                type: 'object',
+              },
+              publicText: {
+                type: 'string',
+              },
+              timeZone: {
+                type: 'object',
+              },
+              trikesAvailable: {
+                type: 'integer',
+                format: 'int32',
+              },
+              latitude: {
+                type: 'object',
+              },
+              longitude: {
+                type: 'object',
+              },
+              _id: {
+                type: 'object',
+              },
+            },
+          },
+          at: {
+            type: 'string',
+            format: 'date-time',
+          },
+          __v: {
+            type: 'integer',
+          },
+        },
+      },
+      weather: {
+        type: 'object',
+        properties: {
+          coord: {
+            type: 'object',
+            properties: {
+              lon: {
+                type: 'number',
+              },
+              lat: {
+                type: 'number',
+              },
+            },
+          },
+          main: {
+            type: 'object',
+            properties: {
+              temp: {
+                type: 'number',
+              },
+              feels_like: {
+                type: 'number',
+              },
+              temp_min: {
+                type: 'number',
+              },
+              temp_max: {
+                type: 'number',
+              },
+              pressure: {
+                type: 'number',
+              },
+              humidity: {
+                type: 'number',
+              },
+            },
+          },
+          wind: {
+            type: 'object',
+            properties: {
+              speed: {
+                type: 'number',
+              },
+              deg: {
+                type: 'number',
+              },
+            },
+          },
+          clouds: {
+            type: 'object',
+            properties: {
+              all: {
+                type: 'number',
+              },
+            },
+          },
+          sys: {
+            type: 'object',
+            properties: {
+              type: {
+                type: 'number',
+              },
+              id: {
+                type: 'integer',
+                format: 'int32',
+              },
+              country: {
+                type: 'string',
+              },
+              sunrise: {
+                type: 'integer',
+                format: 'int32',
+              },
+              sunset: {
+                type: 'integer',
+                format: 'int32',
+              },
+            },
+          },
+          _id: {
+            type: 'string',
+          },
+          weather: {
+            type: 'array',
+            items: {
+              type: 'object',
+              properties: {
+                _id: {
+                  type: 'string',
+                },
+                id: {
+                  type: 'integer',
+                  format: 'int32',
+                },
+                main: {
+                  type: 'string',
+                },
+                description: {
+                  type: 'string',
+                },
+                icon: {
+                  type: 'string',
+                },
+              },
+            },
+          },
+          base: {
+            type: 'string',
+          },
+          visibility: {
+            type: 'integer',
+            format: 'int32',
+          },
+          dt: {
+            type: 'integer',
+            format: 'int32',
+          },
+          timezone: {
+            type: 'integer',
+            format: 'int32',
+          },
+          id: {
+            type: 'integer',
+            format: 'int32',
+          },
+          name: {
+            type: 'string',
+          },
+          cod: {
+            type: 'integer',
+            format: 'int32',
+          },
+          at: {
+            type: 'string',
+            format: 'date-time',
+          },
+          __v: {
+            type: 'integer',
+          },
+        },
+      },
+      at: {
+        type: 'string',
+        format: 'date-time',
+      },
+    },
   },
 };
